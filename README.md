424ce35064c9642e79c612d5286eb751eab6ff21
kevin-amos <kevinongango@gmail.com>
2022-09-18 23:16:35 +0300

    Peform some cleanup, add precommit and linting tools`
    format datetime

---
+++ README.md
@@ -0,0 +1,46 @@
+# yoyo-weather-api
+
+# Payments
+
+The API Service provides a simple API to fetch weather data
+
+The goal of this `README` is to:
+1. Establish a common language to be used by engineers, product managers, and customer success 
+2. Walk engineers through local setup without needing help from others
+
+
+## Common Language
+
+This section establishes ubiquitous language that is accross all stakeholders.  
+This language should be usable by both engineers in code and also other stakeholders.  
+
+Min_temp:  
+Minimum temperature.  
+
+Max_temp:  
+Maximum temperature.  
+
+## Developer Guide
+
+### Local setup
+Install Pipenv locally (Or your preferred virtualization library.)
+Pipenv install
+
+
+### System inputs and outputs
+
+
+###System outputs to external services
+- We make HTTP calls to https://www.weatherapi.com/ for all our weather information.
+
+### Software architecture
+  - To be added. 
+#### Software structure requirements
+
+Conform to the existing layered structure, and use the entities, statics, and repositories in the same manner as existing code.
+
+__Logging__ 
+
+__Test Strategies__ Main regression tests. 
+
+# Learning Resources
--- weather_calculator/api.py
+++ weather_calculator/api.py
@@ -1,26 +1,18 @@
-import logging
-import requests
-from django.shortcuts import render
-from rest_framework.request import Request
-from rest_framework import generics
-from rest_framework.response import Response
 import json
 import logging
-from django.http import (
-    Http404,
-    HttpRequest,
-    HttpResponse,
-    HttpResponseBadRequest,
-    JsonResponse,
-)
 import statistics
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
+
+import requests
 from django.conf import settings
-
-
+from django.http import (Http404, HttpRequest, HttpResponse,
+                         HttpResponseBadRequest, JsonResponse)
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 LOGGER = logging.getLogger(__name__)
-
 
 
 def get_weather_details(request: HttpRequest, city: str) -> JsonResponse:
@@ -39,28 +31,33 @@ def get_weather_details(request: HttpRequest, city: str) -> JsonResponse:
             Return the weather details of the city.
             If an error occurs, its returned instead.
     """
-    days=int(request.GET['days'])
+    days = int(request.GET["days"])
     # import pdb;pdb.set_trace()
-    from_date=datetime.now()-timedelta(days=days)
-    from_date=from_date.strftime("%Y-%m-%d")
-    to_date=datetime.now().strftime("%Y-%m-%d")
-    url=f'http://api.weatherapi.com/v1/history.json?key={settings.WEATHER_API_KEY}&q={city}&dt={from_date}&end_dt={to_date}'
+    from_date = (datetime.now() - timedelta(days=days)).strftime("%Y-%m-%d")
+    to_date = datetime.now().strftime("%Y-%m-%d")
+    url = f"http://api.weatherapi.com/v1/history.json?key={settings.WEATHER_API_KEY}&q={city}&dt={from_date}&end_dt={to_date}"
     response = requests.get(url=url)
-    daily_temepratues=[response.json().get('forecast').get('forecastday')[i].get('day').get('avgtemp_c') for i in range(days)]
-    max_temperature=max(daily_temepratues)
-    min_temperature=min(daily_temepratues)
-    average_temperature=statistics.mean(daily_temepratues)
-    median_temperature=statistics.median(daily_temepratues)
+    daily_temepratues = [
+        response.json()
+        .get("forecast")
+        .get("forecastday")[i]
+        .get("day")
+        .get("avgtemp_c")
+        for i in range(days)
+    ]
+    max_temperature = max(daily_temepratues)
+    min_temperature = min(daily_temepratues)
+    average_temperature = statistics.mean(daily_temepratues)
+    median_temperature = statistics.median(daily_temepratues)
 
     return JsonResponse(
-                {
-                    # "daily_temps": daily_temepratues,
-                    "maximum": max_temperature,
-                    "minimum": min_temperature,
-                    "average": average_temperature,
-                    "median": median_temperature,
-                    # "days" : days,
-                },
-                status=200,
-            )
-
+        {
+            # "daily_temps": daily_temepratues,
+            "maximum": max_temperature,
+            "minimum": min_temperature,
+            "average": average_temperature,
+            "median": median_temperature,
+            # "days" : days,
+        },
+        status=200,
+    )
